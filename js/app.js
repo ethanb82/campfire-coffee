d251447891adbb7214d0ad6394cc821ff260921c
Ethan <ethanleebrown@gmail.com>
2016-01-14 21:39:32 -0800

    Thursday Night Commit

--- js/app.js
+++ js/app.js
@@ -43,191 +43,70 @@ var southLakeUnion = new Kiosk(35, 88, 1.3, 3.7, 'South Lake Union');
 var seaTacAirport = new Kiosk(68, 124, 1.1, 2.7, 'Seatac Airport');
 var websiteSales = new Kiosk(3, 6, 0, 6.7, 'Web Store');
 //
+var sectionEl = document.getElementById('anchor');
+var tableEl = document.createElement('table');
+var rowOne = document.createElement('tr');
+var cellOne = document.createElement('th');
+
+sectionEl.appendChild(tableEl);
+cellOne.textContent = "Location";
+rowOne.appendChild(cellOne);
+tableEl.appendChild(rowOne);
+
+for (var i = 0; i < hours.length; i++){
+  var cell = document.createElement('th');
+  cell.textContent = hours[i];
+  rowOne.appendChild(cell);
+}
+
 function renderTable() {
-  var sectionEl = document.getElementById('anchor');
-  var tableEl = document.createElement('table');
-  var rowOne = document.createElement('tr');
-  var cellOne = document.createElement('th');
-  cellOne.textContent = "Location";
-
-  sectionEl.appendChild(tableEl);
-  tableEl.appendChild(rowOne);
-  rowOne.appendChild(cellOne);
-
-  for (var i = 0; i < hours.length; i++){
-    var cell = document.createElement('th');
-    cell.textContent = hours[i];
-    rowOne.appendChild(cell);
-
-    }
-
+sectionEl;
+console.log(sectionEl);
     for (var x =0; x <allKiosk.length; x++){
       var rows = document.createElement('tr');
       var firstCell = document.createElement('td');
       firstCell.textContent = allKiosk[x].kioskName;
       rows.appendChild(firstCell);
-      for (var y =0; y <hours.length; y++){
+        for (var y =0; y <hours.length; y++){
         var cellTwo = document.createElement('td');
         cellTwo.textContent = allKiosk[x].hourlyBeansByLb[y].toFixed(2);
         rows.appendChild(cellTwo);
+          }
+        tableEl.appendChild(rows);
       }
-      tableEl.appendChild(rows);
-    }
+      console.log("allKiosk before the clear: " + allKiosk);
+      allKiosk = [];
+      console.log("allKiosk after the clear: " + allKiosk);
 }
 
+var newKiosk = document.getElementById('newKiosk');
 
-renderTable();
+newKiosk.addEventListener('submit', handleNewKiosk);
 
+function handleNewKiosk(event) {
+  console.log(event);
+  event.preventDefault();
 
-// function renderTable() {
-//   var tableBody = document.getElementById('tableTwo');
-//   var rowTwo = document.createElement('tr');
-//   var cellTwo = document.createElement('th');
-//   cellTwo.textContent = "Pike Place";
-//   rowTwo.appendChild(cellTwo);
-//   tableBody.appendChild(rowTwo);
-//
-// }
-//
-//
-// renderTable();
-
-
-//my js file will be here //
-// var pikePlace = {
-//   minCust: 14,
-//   maxCust: 55,
-//   avgCupPerCustomer: 1.2,
-//   avgLbsPerCustomer: 3.7,
-//   storeHrs: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],
-//   averageCustomerPerHour: function () {
-//     return Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust;
-//   },
-//   averageCupsPerHour: function() {
-//     return this.averageCustomerPerHour() * this.avgCupPerCustomer;
-//   },
-//   brewedPoundsPerHour: function() {
-//     return (this.averageCupsPerHour() / 20);
-//   },
-//   toGoPoundsPerHour: function() {
-//     return this.averageCustomerPerHour() * this.avgLbsPerCustomer;
-//   },
-//   totalPoundsPerHour:function() {
-//     return this.brewedPoundsPerHour() + this.toGoPoundsPerHour();
-//   }
-// };
-
-// var capHill = {
-//   minCust: 32,
-//   maxCust: 48,
-//   avgCupPerCustomer: 3.2,
-//   avgLbsPerCustomer: 0.4,
-//   storeHrs: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],
-//   averageCustomerPerHour: function () {
-//     return Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust;
-//   },
-//   averageCupsPerHour: function() {
-//     return this.averageCustomerPerHour() * this.avgCupPerCustomer;
-//   },
-//   brewedPoundsPerHour: function() {
-//     return (this.averageCupsPerHour() / 20);
-//   },
-//   toGoPoundsPerHour: function() {
-//     return this.averageCustomerPerHour() * this.avgLbsPerCustomer;
-//   },
-//   totalPoundsPerHour:function() {
-//     return this.brewedPoundsPerHour() + this.toGoPoundsPerHour();
-//   }
-// };
-//
-// var seaPubLib = {
-//   minCust: 49,
-//   maxCust: 75,
-//   avgCupPerCustomer: 2.6,
-//   avgLbsPerCustomer: 0.2,
-//   storeHrs: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],
-//   averageCustomerPerHour: function () {
-//     return Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust;
-//   },
-//   averageCupsPerHour: function() {
-//     return this.averageCustomerPerHour() * this.avgCupPerCustomer;
-//   },
-//   brewedPoundsPerHour: function() {
-//     return (this.averageCupsPerHour() / 20);
-//   },
-//   toGoPoundsPerHour: function() {
-//     return this.averageCustomerPerHour() * this.avgLbsPerCustomer;
-//   },
-//   totalPoundsPerHour:function() {
-//     return this.brewedPoundsPerHour() + this.toGoPoundsPerHour();
-//   }
-// };
-//
-// var SouthLakeUnion = {
-//   minCust: 35,
-//   maxCust: 88,
-//   avgCupPerCustomer: 1.3,
-//   avgLbsPerCustomer: 3.7,
-//   storeHrs: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],
-//   averageCustomerPerHour: function () {
-//     return Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust;
-//   },
-//   averageCupsPerHour: function() {
-//     return this.averageCustomerPerHour() * this.avgCupPerCustomer;
-//   },
-//   brewedPoundsPerHour: function() {
-//     return (this.averageCupsPerHour() / 20);
-//   },
-//   toGoPoundsPerHour: function() {
-//     return this.averageCustomerPerHour() * this.avgLbsPerCustomer;
-//   },
-//   totalPoundsPerHour:function() {
-//     return this.brewedPoundsPerHour() + this.toGoPoundsPerHour();
-//   }
-// };
-//
-// var SeaTac = {
-//   minCust: 68,
-//   maxCust: 124,
-//   avgCupPerCustomer: 1.1,
-//   avgLbsPerCustomer: 2,7,
-//   storeHrs: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],
-//   averageCustomerPerHour: function () {
-//     return Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust;
-//   },
-//   averageCupsPerHour: function() {
-//     return this.averageCustomerPerHour() * this.avgCupPerCustomer;
-//   },
-//   brewedPoundsPerHour: function() {
-//     return (this.averageCupsPerHour() / 20);
-//   },
-//   toGoPoundsPerHour: function() {
-//     return this.averageCustomerPerHour() * this.avgLbsPerCustomer;
-//   },
-//   totalPoundsPerHour:function() {
-//     return this.brewedPoundsPerHour() + this.toGoPoundsPerHour();
-//   }
-// };
-//
-// var webSales = {
-//   minCust: 3,
-//   maxCust: 6,
-//   avgCupPerCustomer: 0,
-//   avgLbsPerCustomer: 6.7,
-//   storeHrs: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],
-//   averageCustomerPerHour: function () {
-//     return Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust;
-//   },
-//   averageCupsPerHour: function() {
-//     return this.averageCustomerPerHour() * this.avgCupPerCustomer;
-//   },
-//   brewedPoundsPerHour: function() {
-//     return (this.averageCupsPerHour() / 20);
-//   },
-//   toGoPoundsPerHour: function() {
-//     return this.averageCustomerPerHour() * this.avgLbsPerCustomer;
-//   },
-//   totalPoundsPerHour:function() {
-//     return this.brewedPoundsPerHour() + this.toGoPoundsPerHour();
-//   }
-// };
+  if (!event.target.minCust.value || !event.target.maxCust.value || !event.target.avgCupPerCustomer.value
+  || !event.target.avgLbsPerCustomer.value || !event.target.kioskName.value) {
+    return alert('Fields cannot be empty!');
+   }
+   var minCust = event.target.minCust.value;
+   var maxCust = event.target.maxCust.value;
+   var avgCupPerCustomer = event.target.avgCupPerCustomer.value;
+   var  avgLbsPerCustomer= event.target.avgLbsPerCustomer.value;
+   var  kioskName= event.target.kioskName.value;
+   console.log("allKiosk before createNewKiosk = " + allKiosk);
+   var createNewKiosk = new Kiosk(minCust, maxCust, avgCupPerCustomer, avgLbsPerCustomer, kioskName);
+   console.log("createNewKiosk makes this: " + createNewKiosk);
+   minCust = "";
+   maxCust = "";
+   avgCupPerCustomer = "";
+   avgLbsPerCustomer = "";
+   kioskName = "";
+  console.log("allKiosk before the push: " + allKiosk);
+    // allKiosk.push(createNewKiosk);
+    console.log("allKiosk.push(createNewKiosk) = " + allKiosk);
+    renderTable();
+ }
+ renderTable();
